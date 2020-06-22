83804231c1ca1d23ac639098aa8ff31e58ed157c
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-06-22 14:10:52 +0300

    Start working

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 console.log("it works")
+
+const artistRecords = [
+    {
+        title: "I wanna be happy",
+        name: "Kirk Franklin",
+        numberOfTrack: 30,
+        url: "https://kirk_franklin/new_album.com",
+        releaseYear: 2010
+    },
+
+    {
+        title: "Seasons in the sun",
+        name: "WestLife",
+        numberOfTrack: 20,
+        url: "https://westlife.com",
+        releaseYear: 2017
+    },
+
+    {
+        title: "Hero",
+        name: "Enrique Iglesias",
+        numberOfTrack: 45,
+        url: "https://enrique_iglesias.com",
+        releaseYear: 2014
+    },
+
+    {
+        title: "Perfect",
+        name: "Ed Sheeran",
+        numberOfTrack: 20,
+        url: "https://ed_sheeran.com",
+        releaseYear: 2015
+    },
+
+    {
+        title: "I won't give up",
+        name: "Jason Mraz",
+        numberOfTrack: 15,
+        url: "https://jason_mraz.com",
+        releaseYear: 2016
+    },
+];
